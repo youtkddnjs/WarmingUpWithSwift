cdd7460b143b061d6c6b07de608afc45d8f2ac54
youtkddnjs <youtkddnjs91@gmail.com>
2024-07-09 13:01:26 +0900

    create project

---
+++ WarmingUpWithSwift/WarmingUpWithSwift/WarmingUpWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarmingUpWithSwiftApp.swift
+//  WarmingUpWithSwift
+//
+//  Created by sangwon on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct WarmingUpWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
